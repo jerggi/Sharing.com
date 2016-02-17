345c1fbfd95163b295b79fae409d450c5f33966f
jerggi <jerggi@gmail.com>
2016-02-17 23:51:24 +0100

    validation of creating category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ActiveRecord::Base
   has_many :items
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates :name, presence: true
 end
