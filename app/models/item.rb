bba191926308e8250c415b70a51b3db9ad61f58c
igor <igor.orsag@gmail.com>
2016-02-17 19:57:23 +0100

    dalsie veci

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,6 @@ class Item < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates :name, presence: true
+  
 end
